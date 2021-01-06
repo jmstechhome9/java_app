c0b5d61788e3ec1f6a3ab28b40a25ff3879921d1
madhu <jmstechhome9@gmail.com>
2021-01-06 15:17:26 +0000

    added more lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,23 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Happy sankranti");
     System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
+    System.out.println("Happy sankranti");
   }
 }
