48e96099830e8b508e1768389ec04054d023778f
madhu <jmstechhome9@gmail.com>
2021-01-07 15:10:36 +0000

    added ugadi line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,10 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Happy sankranti");
     System.out.println("Happy Ugadi.....");
+    System.out.println("Happy Ugadi.....");
+    System.out.println("Happy Ugadi.....");
+    System.out.println("Happy Ugadi.....");
+    System.out.println("Happy Ugadi.....");
+    System.out.println("Happy Ugadi.....");
   }
 }
