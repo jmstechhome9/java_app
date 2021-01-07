03c194785ae1674716067bc261073e548a87e69e
madhu <jmstechhome9@gmail.com>
2021-01-07 14:56:38 +0000

    added ugadi line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,24 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
-    System.out.println("Happy sankranti");
+    System.out.println("Happy Ugadi");
   }
 }
